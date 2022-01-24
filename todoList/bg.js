ffc4984235e553fa3bbc6d393a00fd6f7a926007
jyojun <parkcode98@gmail.com>
2022-01-24 19:15:35 +0900

    index.html 수정

--- todoList/bg.js
+++ todoList/bg.js
@@ -4,7 +4,7 @@ const IMG_NUMBER = 3;
 
 function paintImage(imgNumber){
     const image = new Image();
-    image.src = `img/${imgNumber + 1}.jpg`
+    image.src = `todoList/img/${imgNumber + 1}.jpg`
     image.classList.add("bgImage");
     body.appendChild(image);
 }
